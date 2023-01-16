2cae09de958da9b796b27c975da0c86d0a97fc4a
= <jraynovic@key.net>
2023-01-16 09:39:11 -0800

    temp styling

--- client/src/components/GameBoard.js
+++ client/src/components/GameBoard.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, IconButton } from "@mui/material";
+import { ContentCopy, Check} from '@mui/icons-material/';
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { subscribeToGame } from "../api";
+
 
 const GreenCup = () => {
   return (
@@ -23,10 +26,17 @@ const RedCup = () => {
 const GameBoard = ({ gameId, device, userName }) => {
   const [board, setBoard] = useState([]);
   const [gameInPlay, setGameInPlay] = useState(true);
+  const [copy, setCopy] = useState(false)
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setCopy(false)
+    },[2000])
+  },[copy])
 
   subscribeToGame((err, point) => {
     if (Object.keys(point)[0] === "endgame") setGameInPlay(false);
-    if (device !== point.device && gameId === point.gameId) {
+    if (parseInt(device) !== point.device && gameId === point.gameId) {
       setBoard([...board, point.point]);
     }
   }, device);
@@ -35,46 +45,61 @@ const GameBoard = ({ gameId, device, userName }) => {
     <div>
       <Grid container>
         <Grid className="center" container>
-          <Grid item>
-            <h1>
-              Beer Pong{" "}
-              {`gameId: ${gameId} device: ${device} user name: ${userName}`}
-            </h1>
+          <Grid item className="home-card">
+            <h1>Rad Pong</h1>
           </Grid>
           <Grid item xs={12}>
-            {gameInPlay ? "Still playing" : "User has left the game"}
+            <h4>
+              {gameInPlay ? `Lets win ${userName}!` : `Game ended show score`}{" "}
+              {`Share this game code with a friend ${gameId} to play`}
+              <span>
+                <CopyToClipboard text={gameId} onCopy={() => setCopy(true)}>
+                  <IconButton color="secondary">
+                    {copy ? <Check /> : <ContentCopy />}
+                  </IconButton>
+                </CopyToClipboard>
+              </span>
+            </h4>
           </Grid>
         </Grid>
-        <Grid className="center" container>
-          <Grid item xs={2}>
-            {board.includes(1) ? <RedCup /> : <GreenCup />}
+        <Grid container md={6}>
+          <Grid className="center" container>
+            <Grid item xs={2}>
+              {board.includes(1) ? <RedCup /> : <GreenCup />}
+            </Grid>
+          </Grid>
+          <Grid className="center" container>
+            <Grid item xs={2}>
+              {board.includes(2) ? <RedCup /> : <GreenCup />}
+            </Grid>
+            <Grid item xs={2}>
+              {board.includes(3) ? <RedCup /> : <GreenCup />}
+            </Grid>
+          </Grid>
+          <Grid className="center" container>
+            <Grid item xs={2}>
+              {board.includes(4) ? <RedCup /> : <GreenCup />}
+            </Grid>
+            <Grid item xs={2}>
+              {board.includes(5) ? <RedCup /> : <GreenCup />}
+            </Grid>
+            <Grid item xs={2}>
+              {board.includes(6) ? <RedCup /> : <GreenCup />}
+            </Grid>
           </Grid>
         </Grid>
-        <Grid className="center" container>
-          <Grid item xs={2}>
-            {board.includes(2) ? <RedCup /> : <GreenCup />}
-          </Grid>
-          <Grid item xs={2}>
-            {board.includes(3) ? <RedCup /> : <GreenCup />}
-          </Grid>
-        </Grid>
-        <Grid className="center" container>
-          <Grid item xs={2}>
-            {board.includes(4) ? <RedCup /> : <GreenCup />}
-          </Grid>
-          <Grid item xs={2}>
-            {board.includes(5) ? <RedCup /> : <GreenCup />}
-          </Grid>
-          <Grid item xs={2}>
-            {board.includes(6) ? <RedCup /> : <GreenCup />}
+        <Grid container md={6}>
+          <Grid item md={12}>
           </Grid>
         </Grid>
       </Grid>
+
       <Grid item>
         <Button
           style={{ marginTop: "3rem" }}
           variant="contained"
           onClick={() => setBoard([])}
+          color="secondary"
         >
           Reset
         </Button>
