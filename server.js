82b68800792bfc7e524f480206cccaaa26d2cd93
= <jraynovic@key.net>
2023-01-17 15:57:46 -0800

    changed port config

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ io.on('connection', (socket) => {
 
 
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 sequelize.sync().then(() => console.log("db is ready"));
