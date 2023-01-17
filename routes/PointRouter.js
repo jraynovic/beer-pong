36a745b5433b8103d66e1ba5eb9b6aedae5babf7
= <jraynovic@key.net>
2023-01-17 12:31:43 -0800

    catch error in pointRouter

--- routes/PointRouter.js
+++ routes/PointRouter.js
@@ -1,35 +1,55 @@
-const express = require('express');
-const { Op } = require('sequelize')
+const express = require("express");
+const { Op } = require("sequelize");
 const PointRouter = express.Router();
-const Game = require('../models/GameModel')
-const { makeGameId, makePlayerCode } = require('../helpers/helper');
-const Codes = require('../models/CodesModel');
+const Game = require("../models/GameModel");
+const { makeGameId, makePlayerCode } = require("../helpers/helper");
+const Codes = require("../models/CodesModel");
 
-
-PointRouter.route('/')
-.post(async (req,res)=>{
-  const { device, point } = req.body
-  console.log(req.body)
-  console.log('^^^BODY')
-  const io = req.app.get('io')
-  const game = await Game.findOne({where:{[Op.or]:{deviceOne: parseInt(device), deviceTwo:parseInt(device) }, gameFinished:false } } )
-  if(device === game.dataValues.deviceOne){  
-    if(game.dataValues.playerOnePoints <100){ 
-      const playerOnePoints = +game.dataValues.playerOnePoints +1
-      io.sockets.emit('point', {point:parseInt(point),device, gameId:game.dataValues.gameId})
-      await Game.update({playerOnePoints},{where:{gameId:game.dataValues.gameId}}) 
+PointRouter.route("/").post(async (req, res) => {
+  const { device, point } = req.body;
+  try {
+    const io = req.app.get("io");
+    const game = await Game.findOne({
+      where: {
+        [Op.or]: { deviceOne: parseInt(device), deviceTwo: parseInt(device) },
+        gameFinished: false,
+      },
+    });
+    if (device === game.dataValues.deviceOne) {
+      if (game.dataValues.playerOnePoints < 100) {
+        const playerOnePoints = +game.dataValues.playerOnePoints + 1;
+        io.sockets.emit("point", {
+          point: parseInt(point),
+          device,
+          gameId: game.dataValues.gameId,
+        });
+        await Game.update(
+          { playerOnePoints },
+          { where: { gameId: game.dataValues.gameId } }
+        );
+      }
+      console.log("PLAYER ONE SCORED!");
+    } else if (device === game.dataValues.deviceTwo) {
+      if (game.dataValues.playerOnePoints < 100) {
+        const playerTwoPoints = +game.dataValues.playerOnePoints + 1;
+        io.sockets.emit("point", {
+          point: parseInt(point),
+          device,
+          gameId: game.dataValues.gameId,
+        });
+        await Game.update(
+          { playerTwoPoints },
+          { where: { gameId: game.dataValues.gameId } }
+        );
+      }
+      console.log("PLAYER TWO SCORED!");
     }
-    console.log('PLAYER ONE SCORED!')
-  }else if( device === game.dataValues.deviceTwo){
-    if(game.dataValues.playerOnePoints <100){
-      const playerTwoPoints = +game.dataValues.playerOnePoints +1
-      io.sockets.emit('point', {point:parseInt(point),device, gameId:game.dataValues.gameId})
-      await Game.update({playerTwoPoints},{where:{gameId:game.dataValues.gameId}})
-    }
-    console.log('PLAYER TWO SCORED!')
+    res.status(201);
+    res.send(game);
+  } catch (err) {
+    res.status(400)
+    res.send(err)
   }
-  res.status(201)  
-  res.send(game)
-})
+});
 
-module.exports = PointRouter
+module.exports = PointRouter;
