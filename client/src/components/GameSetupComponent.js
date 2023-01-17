65b9c438afcd9a6d2fe886066217a8d375e457e5
= <jraynovic@key.net>
2023-01-17 15:18:13 -0800

    updated url for production

--- client/src/components/GameSetupComponent.js
+++ client/src/components/GameSetupComponent.js
@@ -14,7 +14,7 @@ const MainComponent = () => {
   useEffect(() => {
     if (gameStarted && joinType === "new") {
       const postNewGame = async () => {
-        const game = await axios.post("http://localhost:5001/game/new", {
+        const game = await axios.post("www.radpong.com/game/new", {
           playerOne: userName,
           deviceOne: device,
         });
@@ -24,7 +24,7 @@ const MainComponent = () => {
     }
     if (gameStarted && joinType === "join") {
       const joinGame = async () => {
-        const game = await axios.post("http://localhost:5001/game/join", {
+        const game = await axios.post("www.radpong.com/game/join", {
           playerTwo: userName,
           deviceTwo: device,
           gameId,
