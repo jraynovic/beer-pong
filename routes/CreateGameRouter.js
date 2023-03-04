fb10f2d6e515db6719798f9246045f230e462434
= <jraynovic@key.net>
2023-03-03 19:51:43 -0800

    game code input can be lower case

--- routes/CreateGameRouter.js
+++ routes/CreateGameRouter.js
@@ -6,8 +6,6 @@ const { makeGameId, makePlayerCode } = require('../helpers/helper');
 GameRouter.route('/new')
 .post(async (req,res)=>{
   const { playerOne, deviceOne } = req.body;
-  console.log(playerOne)
-  console.log*deviceOne
   const io = req.app.get('io');
   const gameId = makeGameId();
   const game = await Game.create({gameId, playerOne, deviceOne})
@@ -22,15 +20,14 @@ GameRouter.route('/join')
 .post(async (req,res)=>{
   const io = req.app.get('io');
   const { playerTwo, gameId, deviceTwo } = req.body;
-  const gameExists = await Game.findOne({where:{gameId,gameFinished:false}})
-  console.log(gameExists, "GAME EXISTS***************\n\n\n")
+  const gameCap = gameId.toUpperCase(); 
+  const gameExists = await Game.findOne({where:{gameId:gameCap,gameFinished:false}})
   if(gameExists === null){
-    return res.send({status:'error',message:`${gameId} does not exist check your game code`})
+    return res.send({status:'error',message:`${gameCap} does not exist check your game code`})
   }
-  const updatedGame = await Game.update({playerTwo, deviceTwo},{where:{gameId,gameFinished:false}})
-  const currentGame = await Game.findOne({where:{deviceTwo, gameId,gameFinished:false}})
-  console.log(currentGame, '\n\n\n\nupdatedGame^^^')
-  const game = {gameId, currentGame}
+  const updatedGame = await Game.update({playerTwo, deviceTwo},{where:{gameId:gameCap,gameFinished:false}})
+  const currentGame = await Game.findOne({where:{deviceTwo, gameId:gameCap,gameFinished:false}})
+  const game = {gameCap, currentGame}
   io.sockets.emit("userAvailable",game);
   res.status(201)  
   res.send({...updatedGame })
